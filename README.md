bd7e200f57359c808733c74ce6a8fb952b1147b4
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-26 21:43:58 -0500

    Initial commit. Added README, and index ox.js. Finished part one of homework.

---
+++ README.md
@@ -0,0 +1,9 @@
+##Introduction
+
+Created a Zoo app with Node.js and mySQL.
+
+Buil with an object.
+
+Using an mySQL database
+
+A command line node app that will take in prompted text as an input.
---
+++ index.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'zoo_db'
+});
+ 
+connection.connect(function(err) {
+  if (err){
+    console.log('err connection ' +err.stack);
+  }else{
+    console.log("I worked");
+  }return
+});
+ 
+var prompt = require('prompt');
+prompt.start();
+prompt.message = ""
