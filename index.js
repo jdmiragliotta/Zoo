57d3a3cda900f739e33de893fc228830e7501b07
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-31 13:00:52 -0500

    Corrected quotation marks

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'zoo_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "zoo_db"
 });
  
 connection.connect(function(err) {
   if (err){
-    console.log('err connection ' +err.stack);
+    console.log("err connection" +err.stack);
   return;
   };
 });
  
-var prompt = require('prompt');
+var prompt = require("prompt");
 prompt.start();
 prompt.message= "";
 
@@ -46,11 +46,11 @@ var zoo = {
   },
 
   visit : function(){
-    console.log("Enter (I): ------> do you know the animal by it's id? We will visit that animal!" + "\r\n" +
-              "Enter (N): ------> do you know the animal by it's name? We will visit that animal!" + "\r\n" +
-              "Enter (A): ------> here's the count for all animals in all locations!" + "\r\n" +
-              "Enter (C): ------> here's the count for all animals in this one city!" + "\r\n" +
-              "Enter (O): ------> here's the count for all the animals in all locations by the type you specified!" + "\r\n" + "\r\n" +
+    console.log("Enter (I): ------> Do you know the animal by it's id? We will visit that animal!" + "\r\n" +
+              "Enter (N): ------> Do you know the animal by it's name? We will visit that animal!" + "\r\n" +
+              "Enter (A): ------> Here's the count for all animals in all locations!" + "\r\n" +
+              "Enter (C): ------> Here's the count for all animals in this one city!" + "\r\n" +
+              "Enter (O): ------> Here's the count for all the animals in all locations by the type you specified!" + "\r\n" + "\r\n" +
               "Enter (Q): ------> Quits to the main menu!)")
   },
 
@@ -84,10 +84,7 @@ var zoo = {
     console.log("Enter animal type to find how many animals we have of that type.");
     prompt.get(["animal_type"], function(err, result){
       connection.query("SELECT COUNT(type) FROM animals WHERE type=?", result.animal_type, function(err, results, fields){
-        if (err){
-          console.log('err connection ' +err.stack);
-          return;
-        };
+        if (err) throw err; 
         console.log("There are "+results[0]["COUNT(type)"]+" "+ result.animal_type+"s at the zoo");
       }); 
       currentScope.menu();
@@ -100,7 +97,7 @@ var zoo = {
     var currentScope = input_scope;
     console.log("Enter city name NY/SF");
       prompt.get(["city_name"], function(err, result) {
-        connection.query('SELECT COUNT(*) AS total FROM animals, caretakers WHERE animals.caretaker_id = caretakers.id AND caretakers.city = ?', [result.city_name], function(err, results, fields) {
+        connection.query("SELECT COUNT(*) AS total FROM animals, caretakers WHERE animals.caretaker_id = caretakers.id AND caretakers.city = ?", [result.city_name], function(err, results, fields) {
         if (err) throw err;
         console.log("Total animals in " +result.city_name+":" + results[0].total);
       });
@@ -113,7 +110,7 @@ var zoo = {
     var currentScope = input_scope;
     console.log("Enter ID of the animal you want to visit");
     prompt.get("animal_id", function(err, result){
-      connection.query('SELECT * FROM animals WHERE id = ?', [result.animal_id], function(err, results, fields) {
+      connection.query("SELECT * FROM animals WHERE id = ?", [result.animal_id], function(err, results, fields) {
         if (err) throw err;
         else {
           console.log("\r\n" +"Animal ID: " + results[0].id  + "\r\n" +
